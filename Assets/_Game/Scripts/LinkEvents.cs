4ce5ff4c6a445c439b6a81161c3d63e634761663
hadv <dinhvietha@savameta.com>
2024-03-16 18:43:24 +0700

    update

--- Assets/_Game/Scripts/LinkEvents.cs
+++ Assets/_Game/Scripts/LinkEvents.cs
@@ -5,6 +5,8 @@
         public static class LinkEvents
         {
             public static GameEvent Click_LevelSelect = new();
-            public static GameEvent Click_MainMenu = new GameEvent();
+            public static GameEvent Click_MainMenu = new ();
+            public static GameEvent Setup_Ship_Completed = new();
+            public static GameEvent Play_End = new ();
         }
     }
