bd658b0317db9c0a35aeae53cd4ea3512751761f
hadv <dinhvietha@savameta.com>
2024-06-10 05:57:19 +0700

    talent tree

--- Assets/_Game/Scripts/Gameplay/TalentTree/TalentNode.cs
+++ Assets/_Game/Scripts/Gameplay/TalentTree/TalentNode.cs
@@ -1,13 +1,36 @@
+using System;
+using UnityEngine;
+
 namespace _Game.Scripts.Gameplay.TalentTree
 {
-    public class TalentNode
+    [ExecuteAlways]
+    public class TalentNode: MonoBehaviour
     {
         public enum Type
         {
+            Normal,
+            Level,
+            Premium,
         }
 
-        public string ItemId;
+        public string itemId;
         public Type nodeType;
 
+        public GameObject lineTop;
+        public GameObject lineRight;
+        public GameObject lineBottom;
+        public GameObject lineLeft;
+
+        private void Awake()
+        {
+            if (nodeType == Type.Normal)
+            {
+                lineLeft.SetActive(false);
+            }
+            else if (nodeType == Type.Premium)
+            {
+                lineRight.SetActive(false);
+            }
+        }
     }
 }
