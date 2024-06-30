53727a7371f29fe9a6c56795abab625436363a52
NgocTao <tntao.khtn@gmail.com>
2024-06-30 13:02:26 +0700

    update

---
+++ Assets/_Game/Features/Equipment/EquipmentScreen.cs
@@ -0,0 +1,12 @@
+using System;
+using Cysharp.Threading.Tasks;
+using ZBase.UnityScreenNavigator.Core.Screens;
+
+namespace _Game.Features.Equipment
+{
+    public class EquipmentScreen : ZBase.UnityScreenNavigator.Core.Screens.Screen
+    {
+
+        
+    }
+}
