01f950739f638acc2bed5a98a591ce263a35cb02
namdinh <namdinhdvh@gmail.com>
2024-07-01 16:46:04 +0700

    add load level design config

---
+++ Assets/_Game/Scripts/GD/LevelDesign/LevelDesignConfig.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+namespace _Game.Scripts.GD
+{
+    [System.Serializable]
+    public class LevelDesignConfig
+    {
+        public string stage;
+        public float time_offset;
+        public string enemy_id;
+
+        public override string ToString()
+        {
+            string ret = stage + " " + enemy_id + " " + time_offset;
+            return ret;
+
+        }
+    }
+}
